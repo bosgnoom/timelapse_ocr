54bd758f130cf29f2af83711f460ee425a82313c
bosgnoom <paul.schouten@gmail.com>
2018-01-14 09:14:58 +0100

    Attempt to determine the timestamp

--- make_timelapse.py
+++ make_timelapse.py
@@ -1,63 +1,72 @@
+#!/usr/bin/python
+
+"""
+    Make a timelapse video from timelapsed AVI files
+    Detect timestamp of each frame
+    Determine whether to include this frame in the final movie
+    Calculate an averaged frame
+    Invoke ffmpeg to make an h264 encoded file
+"""
+
+# Import modules
 from imutils import contours
-import numpy as np
-#import argparse
 import imutils
+import numpy as np
 import cv2
 
-#load reference image
-ref = cv2.imread('cijfers.png')
-ref = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY)
-ref = cv2.threshold(ref, 10, 255, cv2.THRESH_BINARY)[1]
+def load_reference_image(name):
+    # First, load reference image: this image contains the figures 0123456789
+    reference = cv2.imread(name)
+    reference = cv2.cvtColor(reference, cv2.COLOR_BGR2GRAY)
+    reference = cv2.threshold(reference, 10, 255, cv2.THRESH_BINARY)[1]
 
-#cv2.imshow("ref", ref)
+    # Find the figures in the reference image
+    reference_contours = cv2.findContours(reference.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # There's a difference in opencv API versions, account for that
+    reference_contours = reference_contours[0] if imutils.is_cv2() else reference_contours[1]
+    reference_contours = contours.sort_contours(reference_contours, method="left-to-right")[0]
 
-# Find the figures in the reference image
-refcnts=cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-refcnts=refcnts[0] if imutils.is_cv2() else refcnts[1]
-refcnts=contours.sort_contours(refcnts, method="left-to-right")[0]
-digits={}
+    # Put each digit in a dict
+    digits={}
+    for (i, c) in enumerate(reference_contours):
+        (x, y, w, h) = cv2.boundingRect(c)
+        roi = reference[y:y+h, x:x+w]
+        digits[i]=roi
 
-# Put each digit in a dict
-for (i, c) in enumerate(refcnts):
-    (x, y, w, h) = cv2.boundingRect(c)
-    roi = ref[y:y+h, x:x+w]
-    #cv2.imshow(" roi", roi)
-    #cv2.waitKey(0)
-    digits[i]=roi
+    # Return dict
+    return digits
 
-#print(digits)
-#rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 1))
+def find_timestamp(digits):
+    # open video file
+    cap = cv2.VideoCapture('TLC00032.AVI')
 
-#image = cv2.imread('cijfers.png')
-#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#cv2.imshow("grijs", gray)
+    # if video file opened, grab a frame
+    if cap.isOpened():
+        ret, frame = cap.read()
+        # Convert to greyscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# open video file
-cap = cv2.VideoCapture('TLC00032.AVI')
+        # Find digits in the image by matchTemplate each digit
+        output=[]
+        for number, digit in digits.items():
+            result = cv2.matchTemplate(gray, digit, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.9
+            locations = np.where(result >= threshold)
 
-# if video file opened, grab a frame
-if cap.isOpened():
-    ret, frame = cap.read()
-    #cv2.imshow('rgb', frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow('frame', gray)
-
-    output=[]
-    for number, digit in digits.items():
-        res = cv2.matchTemplate(gray, digit, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.9
-        loc = np.where(res >= threshold)
-
-        #print(loc[1])
-        for location in loc[1]:
-            output.append([location, number])
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(frame, pt, (pt[0]+w, pt[1]+h), (0,255,0), 1)
+            for position in np.transpose(locations):
+                if position[0] == 704:
+                    output.append([number, position[1]])    
                       
-    cv2.imshow('frame', frame)                  
-    cv2.waitKey(0)
+    # close video file
+    cap.release()
 
-cap.release()
-cv2.destroyAllWindows()
+    print(output)
 
-print(output)
+def main():
+    print("Starting...")
+    numbers = load_reference_image('cijfers.png')
+    find_timestamp(numbers)
+
+if __name__ == "__main__":
+    main()
+    
