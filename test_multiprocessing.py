df781e6cdcf56e65643b3ecd27034c64044732b9
bosgnoom <paul.schouten@gmail.com>
2018-01-20 11:51:58 +0100

    testing multiprocessing

--- test_multiprocessing.py
+++ test_multiprocessing.py
@@ -3,17 +3,24 @@
 import multiprocessing
 import time
 import os
+import timeit
 
 
 def doet_het_werk(a, b):
-    antwoord = "Testing... {}-{}-{}".format(a, os.getpid(), multiprocessing.current_process())
+    antwoord = "Testing... {}-{}-{}".format(a, b, multiprocessing.current_process().name)
+    print(antwoord)
+    time.sleep(1)
     return antwoord
 
 
-if __name__ == "__main__":
-    piep_piep = ["Ja", "Nee", "Misschien"]
-    supr = [ [x, "iets"] for x in piep_piep ]
+def main():
+    piep_piep = ["Ja", "Nee", "Misschien", "Nog wat", "zo meer", "laatste"]
+    supr = [[x, "iets"] for x in piep_piep]
 
     with multiprocessing.Pool() as pool:
-          results = pool.starmap(doet_het_werk, supr)
+        results = pool.starmap(doet_het_werk, supr)
     print(results)
+
+
+if __name__ == "__main__":
+    print(timeit.timeit(main, number=1))
